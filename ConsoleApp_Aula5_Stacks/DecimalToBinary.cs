a1759c2503eeaeb0f1e67b8d64252d3b16ea1a75
VitorMPCastro <152223916+VitorMPCastro@users.noreply.github.com>
2024-04-08 17:59:01 -0300

    Add project files.

---
+++ ConsoleApp_Aula5_Stacks/DecimalToBinary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Aula5_Stacks
+{
+    internal class DecimalToBinary
+    {
+        public static void Convert(int number)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (number > 0)
+            {
+                stack.Push(number % 2);
+                number /= 2;
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
